e885678219ca522fae1dfc3653490e0ef3ef7a50
Dmytro Zhyvonitko <d.zhyvonitko@protonmail.com>
2022-02-23 23:48:38 +0200

    add procfile

--- application.py
+++ application.py
@@ -1,13 +1,12 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
 
 application = Flask(__name__)
 
 
 @application.route("/", methods=["GET"])
-def index():
-    return "hello world"
+def app():
+    return make_response(jsonify({"message": "hello world"}))
 
 
-if __name__ == "main":
-    application.debug = True
-    application.run(load_dotenv=True)
+if __name__ == "__main__":
+    application.run()
